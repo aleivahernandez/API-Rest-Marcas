3afaeff44844fb85240d558a547996bba329fbb2
aleivahernandez <aleiva@inapi.cl>
2025-05-07 17:03:42 -0400

    Update app.py

--- app.py
+++ app.py
@@ -16,4 +16,5 @@ def buscar_marcas():
     return jsonify({'resultados_sbert': resultados_sbert, 'resultados_beto': resultados_beto})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
